16a814d31879f94790dddca0135cb28e24ed5642
Minh Hang Nguyen <hang00103@gmail.com>
2021-09-21 16:23:25 -0400

    modified readFile() to convert md syntax to html syntax

--- bin/helpers/readFile.js
+++ bin/helpers/readFile.js
@@ -1,8 +1,12 @@
+const path = require("path");
+
 module.exports.readFile = (file) => {
   let results;
   try {
     results = require("fs").readFileSync(file, "utf-8");
-
+    if (path.extname(file) === ".md") {
+      results = results.replace(/^# (.*$)/gim, "<h1>$1</h1>");
+    }
     return results.split(/\r?\n\r?\n/).map((e) => e.replace(/\r?\n/, " "));
   } catch (err) {
     // TODO: check for errors before parsing when giving an invalid file
